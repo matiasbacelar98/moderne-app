849d90de44f5e824a62e3a58a91fb2225e5f7810
matias bacelar <matiasbacelar@gmail.com>
2021-12-27 17:54:46 -0300

    configuracion router completa

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+import GlobalStyles from './styles/globalStyles';
+import Home from './routes/home/Home';
+import About from './routes/about/About';
+import Properties from './routes/properties/Properties';
+import SinglePropertie from './routes/singlePropertie/SinglePropertie';
+import NotFound from './routes/404/NotFound';
+
 function App() {
-  return <div>working...</div>;
+  return (
+    <>
+      <GlobalStyles />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sobre-moderne' element={<About />} />
+        <Route path='/propiedades' element={<Properties />} />
+        <Route path='/propiedades/:id' element={<SinglePropertie />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
