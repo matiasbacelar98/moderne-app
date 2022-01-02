e451487bf1f44bf6063b95af8dc4a2f73bfc6570
matias bacelar <matiasbacelar@gmail.com>
2022-01-02 00:36:30 -0300

    componente quality

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,5 +1,17 @@
+import Qualities from '../../components/qualities/Qualities';
+import { qualityOne, qualityTwo, qualityThree } from '../../utils/qualitiesData';
+
 const Home = () => {
-  return <div>home...</div>;
+  return (
+    <div
+      className='wrapper flow-spacing header-spacing-top footer-spacing-bottom'
+      style={{ height: '1000vh', paddingTop: '80rem' }}
+    >
+      <Qualities values={qualityOne} />
+      <Qualities values={qualityTwo} reverse />
+      <Qualities values={qualityThree} />
+    </div>
+  );
 };
 
 export default Home;
